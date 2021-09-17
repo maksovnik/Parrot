14936255d7a85149fe3780c75dfa68ba7ddebb91
Maks <maksovni@gmail.com>
2021-09-18 00:50:24 +0100

    video ren working

--- index.js
+++ index.js
@@ -1,235 +1,207 @@
 const videoGrid = document.getElementById("video-grid");
+
 const iceConfig = {
-    iceServers: [{
-        urls: 'stun:stun.l.google.com:19302'
-    }]
+	iceServers: [{
+		urls: 'stun:stun.l.google.com:19302'
+	}]
 }
-var connection;
+
 var sock;
 
-
-var localstream;
+cameraOn = false;
 document.title = "Parrot"
 
+var sender;
 
-const id2content = {};
+var video;
+
 const remoteStreams = []
 
 function send(msg, type) {
-    if(sock.readyState!=WebSocket.OPEN){
-        return;
-    }
-    const payload = {
-        action: type,
-        msg
-    }
-    console.log(JSON.stringify(payload))
-    sock.send(JSON.stringify(payload))
+	if (sock.readyState != WebSocket.OPEN) {
+		return;
+	}
+	const payload = {
+		action: type,
+		msg
+	}
+	console.log("Message sent")
+	sock.send(JSON.stringify(payload))
 }
 
-
-function hideBox() {
-    document.getElementById("setup").style.visibility = "hidden";
+function contains(x, y) {
+	if (y.indexOf(x) === -1) {
+		return false;
+	} else {
+		return true
+	}
 }
+
 async function generate() {
 
-   var room = document.getElementById('room').value
+	var room = document.getElementById('room').value
 
 
-   sock = new WebSocket('ws://127.0.0.1:8765')
+	sock = new WebSocket('ws://127.0.0.1:8765')
 
 
-    sock.addEventListener('close', e => console.log('Socket is closed'))
+	sock.addEventListener('close', e => console.log('Socket is closed'))
 
-    sock.addEventListener('error', e => console.error('Socket is in error', e))
+	sock.addEventListener('error', e => console.error('Socket is in error', e))
 
-    sock.addEventListener('message', e => {
-        try{
-            data=JSON.parse(e.data)
-            console.log('New Message:', data)
-            if(data.error==="room taken"){
-                sock.close()
-                connection.close();
-            }
-        }
-        catch(e){
-            console.log('New Message:', e.data)
-        }
-        
-    })
+	sock.addEventListener('message', e => {
+		try {
+			data = JSON.parse(e.data)
+			console.log('New Message:', data)
+		} catch (e) {
+			console.log('New Message:', e.data)
+		}
 
-    sock.addEventListener('open', async e => {
-        console.log('Socket is connected')
-
-        
-
-        hideBox()
-        connection = new RTCPeerConnection(iceConfig);
-        var options = {};
-
-        if (document.getElementById('camera').checked) {
-            console.log("Camera enabled")
-            options["video"] = true;
-        }
-
-        if (document.getElementById('microphone').checked) {
-            console.log("Mic enabled")
-            options["audio"] = {
-                autoGainControl: false,
-                channelCount: 2,
-                echoCancellation: false,
-                latency: 0,
-                noiseSuppression: false,
-                sampleRate: 48000,
-                sampleSize: 16,
-                volume: 1.0
-            };
-        }
-
-        camera = await navigator.mediaDevices.getUserMedia(options)
-
-        addStream(camera,true)
-        id2content[camera.id] = 'webcam'
-        camera.getTracks().forEach(track => {
-            console.log("Added Camera track to connection")
-            connection.addTrack(track,camera)
-        
-        })
-
-        if (document.getElementById('screen').checked) {
-            console.log("Screen enabled")
-            localStream = await navigator.mediaDevices.getDisplayMedia({
-                audio: true,
-                video: true
-            })
-
-            id2content[localStream.id] = 'screen'
-            localStream.getTracks().forEach(track => {
-                console.log("Added Stream track to connection")
-                //connection.addTrack(track,localStream)
-            })
-        }
+	})
 
 
-        connection.onconnectionstatechange = async e=>{
-            if(connection.connectionState === 'connected'){
-                console.log("Connected")
-                document.getElementById("status").style.visibility = "hidden";
+	sock.addEventListener('open', async e => {
+		console.log('Socket is connected')
 
-                const stats = await connection.getStats()
-                let selectedLocalCandidate;
-                for (const {type, state, localCandidateId} of stats.values())
-                    if (type === 'candidate-pair' && state === 'succeeded' && localCandidateId) {
-                        selectedLocalCandidate = localCandidateId
-                        break
+
+		connection = new RTCPeerConnection(iceConfig);
+
+		connection.onnegotiationneeded = e => {
+			console.log("Now!!!!")
+		}
+
+		var options = {};
+
+		console.log("Mic enabled")
+		options["audio"] = {
+			autoGainControl: false,
+			channelCount: 2,
+			echoCancellation: false,
+			latency: 0,
+			noiseSuppression: false,
+			sampleRate: 48000,
+			sampleSize: 16,
+			volume: 1.0
+		}
+
+		camera = await navigator.mediaDevices.getUserMedia(options)
+
+		connection.addTrack(camera.getTracks()[0], camera)
+
+		connection.onconnectionstatechange = async e => {
+			if (connection.connectionState === 'connected') {
+				console.log("Connected")
+			}
+		}
+
+		connection.onicecandidate = e => {
+			console.log("New ICE Candidate!")
+		}
+
+
+		connection.onicegatheringstatechange = e => {
+			if (connection.iceGatheringState === 'complete') {
+				console.log("Ice Gathering complete")
+				var sdp = connection.localDescription.toJSON()
+				send(sdp, 'signal')
+			}
+		}
+
+
+
+
+		connection.ontrack = async event => {
+
+			console.log("Track recieved")
+			var track = event.track;
+			stream = event.streams[0]
+
+			if (!contains(stream, remoteStreams)) {
+				remoteStreams.push(stream);
+                
+				video = document.createElement('video')
+
+				video.srcObject = stream;
+				video.controls = true;
+
+				video.play();
+
+				videoGrid.append(video)
+
+                stream.onremovetrack = k =>{
+                    video.srcObject = stream;
+                    video.play();
+                }
+
+
+			}
+
+			stream.addTrack(track);
+            console.log(stream.getTracks().length)
+		}
+
+
+		send({
+			'room': room
+		}, "joinRoom");
+
+
+
+
+
+		sock.addEventListener('message', async e => {
+			var o = JSON.parse(e.data)
+			var s = o.message
+			if (s === 'roomJoined') {
+                
+                document.getElementById('camera').onclick = async e=>{
+                    if(cameraOn==false){
+                        cameraOn = true;
+                        var stream = await navigator.mediaDevices.getUserMedia({"video":true})
+                        sender = connection.addTrack(stream.getVideoTracks()[0],camera)
+
                     }
-                    var t1 = performance.now()
-                console.log(stats.get(selectedLocalCandidate).candidateType)
-            
-            }
-        }
-
-        connection.onicecandidate = e => {
-            console.log("New ICE Candidate!")
-        }
-
-        
-        connection.onicegatheringstatechange = e => {
-            if (connection.iceGatheringState === 'complete') {
-                // if(peerA){
-                //     document.getElementById("status").style.visibility = "visible";
-                // }
-                console.log("Ice Gathering complete")
-                var t = connection.localDescription
-                var p = t.toJSON()
-                p["meta"] = id2content;
-                console.log("SDP has been sent")
-                send(p, 'signal')
-
-                // connection.onicecandidate = ({candidate}) => {
-                //     console.log(candidate)
-                //     //io.send({candidate})
-                // };
+                    else{
+                        cameraOn = false;
+                        await connection.removeTrack(sender);
+                        connection.restartIce()
+                    }
+                    await connection.setLocalDescription(connection.createOffer())
 
                 }
 
-                
-        }
+
+				var clientId = o.order
+
+				if (clientId == 1) {
+					connection.setLocalDescription()
+				}
 
 
-        connection.ontrack = event => {
-            var track = event.track;
-            var stream = event.streams[0]
-            
-            if (remoteStreams.indexOf(stream) === -1){
-                remoteStreams.push(stream);
-                addStream(stream)
-            }
 
-            stream.addTrack(track);
-        }
+				sock.addEventListener('message', async e => {
+					var s = JSON.parse(e.data)
+					if (s.type === 'offer' || s.type === 'answer') {
+						connection.setRemoteDescription(s).then(a => console.log("Remote Description set"))
+
+						if (clientId == 2) {
+							connection.setLocalDescription()
+						}
 
 
-        send({'room':room},"joinRoom");
-
-        sock.addEventListener('message', async e => {
-            var o = JSON.parse(e.data)
-            var s = o.message
-            if(s==='roomJoined'){
-
-                var clientId = o.order
-
-                if(clientId==1){
-                    connection.setLocalDescription()
-                }
-                
-                connection.onnegotiationneeded = async () => {
-                    await connection.setLocalDescription(await connection.createOffer());
-
-                    var t = connection.localDescription
-                    var p = t.toJSON()
-                    p["meta"] = id2content;
-                    console.log("SDP has been sent")
-                    send(p, 'signal')
-
-                  }
-                
-                sock.addEventListener('message', async e => {
-                    var s = JSON.parse(e.data)
-                    if(s.type==='offer'||s.type==='answer'){
-                        connection.setRemoteDescription(s).then(a => console.log("Remote Description set")) 
-        
-                        if(clientId==2){
-                            connection.setLocalDescription()
-                        }
-                    }
-                    
-        
-                    
-                })
-            }
-        })
-
-        
-
-        
+					}
 
 
-    })
 
-}
+				})
+			}
+		})
 
-function addStream(stream, muted) {
-    var video = document.createElement('video')
 
-    video.srcObject = stream;
-    video.controls = true;
 
-    if (muted) {
-        video.muted = true;
-    }
 
-    video.play();
 
-    videoGrid.append(video)
+
+	})
+
 }
