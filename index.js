a755f6ff700e24d46ce44b79e0031522127dc350
Maks <maksovni@gmail.com>
2021-09-19 22:27:12 +0100

    Revert "started working on local streams, many bugs"
    This reverts commit 60b12a97cf1b6f1fc773c5724ff5c4b7a4af5dfd.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const videoGrid = document.getElementById("video-grid");
-const localVideoGrid = document.getElementById("localvideo-grid");
 
 const iceConfig = {
     iceServers: [{
@@ -13,8 +12,6 @@ document.title = "Parrot"
 
 var sock;
 var camera;
-var screen;
-
 var remoteStreams = []
 var streamToVideo = {}
 
@@ -59,11 +56,7 @@ function contains(x, y) {
 function switchDisplay() {
     document.getElementById("camera").style.display = '';
     document.getElementById("screen").style.display = '';
-
-}
-
-function switchone(){
-	document.getElementById("generate").style.display = 'none';
+    document.getElementById("generate").style.display = 'none';
     document.getElementById("label").style.display = 'none';
     document.getElementById("room").style.display = 'none';
 }
@@ -84,60 +77,44 @@ function updateButton(value,id){
 	document.getElementById(id).innerHTML = value +" "+ id
 }
 
-function createSource(id,func) {
+function createSource(id) {
 	this.on = false
 	this.senders = []
 
 	document.getElementById(id).onclick = async e => {
-		
-
 		if (this.on == false) {
-			var source = await func();
+
 			updateButton('Disable',id)
+			if (id === 'screen') {
+				var stre = await getScreen();
+			}
+			if (id === 'camera') {
+				var stre = await getCamera();
+			}
 
-			var tracks = source.getTracks()
+			var tracks = stre.getTracks()
 			tracks.forEach(track => {
-				if(id==='camera'){
-					source = camera;
-					
-				}
-				source.addTrack(track)
-				var sender = connection.addTrack(track, source)
-				this.senders.push(sender)
-				updateVideo(streamToVideo[source],source)
 
-				//convert to lambda that runs delete method if deleted for
-				// example onDelete => removetrack(sender)
+				if (id === 'screen') {
+					var sender = connection.addTrack(track, stre)
+				}
+				if (id === 'camera') {
+					var sender = connection.addTrack(track, camera)
+				}
+
+				this.senders.push(sender)
 			})
 
 
 		} else {
 			updateButton('Enable',id)
 			this.senders.forEach(s => connection.removeTrack(s))
-			if(id=='camera'){
-				await camera.removeTrack(camera.getVideoTracks()[0])
-				updateVideo(streamToVideo[camera],camera)
-			}
-			if(id=='screen'){
-				screen.getTracks().forEach(track =>{
-					screen.removeTrack(track)
-				})
-				deleteVideo(streamToVideo[screen])
-			}
-
-			
-
 
 		}
+
 		this.on = !this.on;
 
-		if(connection.connectionState==='connected'){
-			connection.setLocalDescription(await connection.createOffer({iceRestart: true}))
-		}
-
-
-
-		
+		connection.setLocalDescription(await connection.createOffer({iceRestart: true}))
 	}
 }
 
@@ -158,7 +135,7 @@ function getAudioOptions(){
 async function connect() {
 
     var room = document.getElementById('room').value
-    
+    switchDisplay();
 
     sock = new WebSocket('ws://127.0.0.1:8765')
 
@@ -218,7 +195,7 @@ function createStream(stream){
 
 	stream.onremovetrack = k => {
 		if (stream.getTracks().length == 0) {
-			deleteVideo(video)
+			deleteVideo(video)``
 		} else {
 			updateVideo(video,stream)
 		}
@@ -240,18 +217,14 @@ function onTrack(track,stream){
 
 async function open(){
 	console.log('Socket is connected')
-	switchone();
 
 	connection = new RTCPeerConnection(iceConfig);
 
-	var d = new createSource('camera',getCamera);
-	var c = new createSource('screen',getScreen);
+
 	console.log("Microphone enabled")
 	camera = await navigator.mediaDevices.getUserMedia(getAudioOptions())
 
 	connection.addTrack(camera.getTracks()[0], camera)
-
-	onTrack(camera.getAudioTracks()[0],camera)
 
 	connection.onconnectionstatechange = async e => {
 		if (connection.connectionState === 'connected') {
@@ -275,15 +248,14 @@ async function open(){
 
 
 	send({'room': room}, "joinRoom");
-	switchDisplay();
+
 	sock.addEventListener('message', async e => {
 		var o = JSON.parse(e.data)
 		var s = o.message
 		if (s === 'roomJoined') {
 
-			
-
-
+			var d = new createSource('camera');
+			var c = new createSource('screen');
 
 			var clientId = o.order
 
