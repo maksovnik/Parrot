681452c7201d88e97de1877a8c9d32ffb5c65f8c
Maks <maksovni@gmail.com>
2023-04-16 18:49:22 +0100

    simple page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile('views/index.html', {root: __dirname });
 });
 
 app.listen(process.env.PORT || 5000, () => {
